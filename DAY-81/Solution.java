feb15357c59580a3935479326df408680ac867de
SANJAY R <sanjayias91@gmail.com>
2026-02-21 22:36:49 +0530

    leetcode daily challenge - day 81

---
+++ DAY-81/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+
+        for (int i = left; i <= right; i++) {
+            int setBits = countSetBits(i);
+
+            if (isPrime(setBits)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int countSetBits(int num) {
+        int bits = 0;
+
+        while (num > 0) {
+            if (num % 2 == 1) {
+                bits++;
+            }
+            num /= 2;
+        }
+
+        return bits;
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
